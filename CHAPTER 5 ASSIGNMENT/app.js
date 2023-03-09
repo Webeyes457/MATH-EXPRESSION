9a2627197ca3cff9cb55b37400edb272f9974c33
Muhammad Murtaza <121543125+Webeyes457@users.noreply.github.com>
2023-03-09 08:53:49 +0500

    Initial

---
+++ CHAPTER 5 ASSIGNMENT/app.js
@@ -0,0 +1,91 @@
+
+alert("CHAPTER 5 ASSIGNMENT")
+
+//                         ++ADDITION++
+
+        var num1 = 90;
+        var num2= 90;
+        var total= num1+num2;
+
+        alert("THE ADDITION OF " +num1+ " and " +num2+ " is " + total)
+
+ //                       --SUBTRACTION--
+
+        var num1= 90;
+        var num2= 90;
+        var total= num1-num2;
+              
+
+        alert(" THE SUBTRACTION OF " + num1+ " AND " + num2+ " IS = " +total)
+
+ //                        // DIVISION//*
+        var num1= 90;
+        var num2= 90;
+        var total= num1/num2;
+        alert(" THE DIVISION OF " +num1+ " AND " +num2+ " IS = " +total)
+                    
+
+
+//                            %%MODULUS%%
+
+        var num1= 90;
+        var num2= 90;
+        var total= num1%num2;
+
+        alert(" THE Modulus OF " +num1+ " AND " +num2+ " IS = " +total )
+
+//                           **MULTIPLICATION**
+
+        var num1= 90;
+        var num2= 90;
+        var total= num1*num2;
+
+        alert(" THE MULTIPLIICATION OF " +num1+ " AND " +num2+ " IS = " +total);
+
+
+                        //  ??  NEXT QUESTION   ??
+
+
+var a = 5;
+a = ++a
+alert("BEFORE INCREMNT a is 5 \n AND AFTER INCREMENT a is "+ a );
+
+
+a=a+7;
+
+alert("AFTER ADDING 7 IN VARIBLE a THE VALUE OF VARIABLE a IS " 
++a)
+
+a=--a
+
+alert("AFTER DECREMENT IN VARIBLE a THE VALUE OF VARIABLE a IS " 
++ a)
+
+
+a=a%3
+
+alert("AFTER MODULUS VARIBLE a BY 3 THE VALUE OF VARIABLE a IS " 
++ a)
+
+
+document.write(" <h3>  VALUE AFTER VARIABLE DECLARATION IS UNDEFINED <br>INITIAL VALUE IS :5 <br> AFTER INCREMENT IS :6 <br> VALUE AFTER ADDING IS :13 <br> VALUSE AFTER DECREMENT IS :12 <br> THE REMINDER IS :O <br> COST OF ONE MOVIE TICKET IS 600 <br> TOTAL COST TO BUY 5 TICKETS TO A MOVIE IS 3000pkr  </h3>");
+
+
+
+//                           **NEXT QUESTION**
+
+
+
+var ticketCost=600
+
+alert(" COST OF ONE MOVIE TICKET IS   " + ticketCost)
+alert("TOTAL COST TO BUY 5 TICKETS TO A MOVIE IS "+ ticketCost*5 + " PKR"
+)
+
+//                                Tables
+
+
+var num=prompt("Enter a num")
+
+            document.write(num+ "  x " + " 1 " + "= "+ num*1+ "<br>" + num+ " x " + " 2" + "= "+ num*2+ "<br>"+num+ "  x " + " 3 " + "= "+ num*1+ "<br>" +num+ " x " + " 4 " + "= "+ num*1+ "<br>"+num+ "  x " + " 5 " + "= "+ num*1+ "<br>"+num+ " x " + " 6 " + "= "+ num*1+ "<br>"+num+ "  x " + " 7 " + "= "+ num*1+ "<br>"+num+ " x " + " 8 " + "= "+ num*1+ "<br>"+num+ "  x " + " 9 " + "= "+ num*1+ "<br>"+num+ " x " + " 10 " + "= "+ num*1
+            )
